0a15713fe70fc7e004d594ad20721a114d5cf507
Abhinav <abhinavbhardwaj510@gmail.com>
2021-05-18 17:56:44 +0530

    Updated client file

--- client.py
+++ client.py
@@ -1,20 +1,33 @@
 import socket
 
-tcp_port = 66 # Insert a Free Port as per your choice
-tcp_ip = 'xx.xx.xx.xx' # Insert IP Address
+tcp_port = 66
+tcp_ip = '127.0.0.1'
 buf_size = 30
-msg = "Hello World"
+
+print("[INFO] Creating Socket...")
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("[INFO] Socket successfully created")
+
+print("[INFO] Connecting Socket to port",tcp_port)
+s.connect((tcp_ip,tcp_port))
+print("[INFO] Socket connected successfully to port",tcp_port)
+
+msg = "Hello Cruel World"
+print("[INFO] Encoding data...")
 msg = msg.encode('utf-8')
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("Socket successfully created")
-
-s.connect((tcp_ip,tcp_port))
-print("Socket is connected to",tcp_port)
-
+print("[INFO] Sending data to Server...")
 s.send(msg)
+print("[INFO] Data sent successfully to Server")
+
+print("[INFO] Receiving Data from server")
 data = s.recv(buf_size)
+
+print("[INFO] Decoding received data...")
 data = data.decode('utf-8')
 
-print('Data received from Server : ',data)
+print('[INFO] Data received from Server : ',data)
+
+print("[INFO] Disconnecting Socket...")
 s.close()
+print("[INFO] Socket disconnected successfully")
